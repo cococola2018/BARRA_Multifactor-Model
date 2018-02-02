0c355f3e55092ce33222bf0e4ef6fd1505c892c6
cococola2018 <36035748+cococola2018@users.noreply.github.com>
2018-02-02 21:48:09 +0800

    Update Data_PreProcess.m

--- Data_PreProcess.m
+++ Data_PreProcess.m
@@ -1,3 +1,4 @@
+
 %*******************读入原始数据*********************
 function [Raw_Data_Stock,Raw_Date_Factor]=Load_Data(start_date)
 
@@ -45,6 +46,43 @@ std_value=std(Data,0,2,'omitnan');
 
 Data(Data>mean_value+3*std_value)=nan;
 Data(Data<mean_value-3*std_value)=nan;
+New_Data=Data;
+
+end
+
+%*******************处理缺失值*********************
+%处理方法包括直接删去缺失值，用每只股票的一段时间的均值替代，或用中位数替代
+%MSCI USE4提出了用回归的方法填补缺失值，这里觉得不合适，暂未写代码，可留之后开发
+function [New_Data]=MissingData_Replacement(Data,way)
+
+Data(Data==0)=nan;%if no way is inputed, then we will simply delete the missing data
+[~,num_stock]=size(Data);
+
+if way=='insert_mean'
+    Data(Data==0)=nan;
+    mean_value=mean(Data,1,'omitnan');
+    [~,num_stock]=size(Data);
+    for i=1:num_stock
+        data=Data(:,i);
+        data(isnan(data))=mean_value(i);
+        Data(:,i)=data;
+    end
+end
+
+if way=='insert_median'
+    median_value=median(Data,1,'omitnan');
+
+    for i=1:num_stock
+        data=Data(:,i);
+        data(isnan(data))=median_value(i);
+        Data(:,i)=data;
+    end
+end
+
+%'Regress' way could also be added:regress non-missing exposures against a
+%subset of factors, with the slope coefficients being used to estimate the factor exposures for the stocks
+%with missing data.
+
 New_Data=Data;
 
 end
